438361d9b0f56953b3a07881fb7e3c7aa9e68ae6
Marcin Sikora <sikomar@interia.pl>
2023-03-31 18:54:18 +0200

    feat: [TASK-11346] Dog modify in main branch

--- Dog.java
+++ Dog.java
@@ -1,6 +1,9 @@
 public class Dog {
     public static void main(String[] args) {
         System.out.println("jestem piesem");
+        System.out.println("jestem piesem");
+        System.out.println("jestem piesem");
+        System.out.println("jestem piesem");
     }
 
 
