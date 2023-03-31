ba62b28ba16efeb1659bad08383f2a5477cd7324
Marcin Sikora <sikomar@interia.pl>
2023-03-31 20:16:52 +0200

    Merge branch 'nowyBranch'
    # Conflicts:
    #	Dog.java

--- Dog.java
+++ Dog.java
@@ -1,13 +1,12 @@
 public class Dog {
     public static void main(String[] args) {
-        System.out.println("jestem piesem");
-        System.out.println("jestem piesem");
-        System.out.println("jestem piesem");
-        System.out.println("jestem piesem");
-        System.out.println("jestem piesem");
-        System.out.println("jestem piesem");
+        System.out.println("jestem piesem w mainie");
+        System.out.println("jestem piesem w mainie");
+        System.out.println("jestem piesem w mainie");
+        System.out.println("jestem piesem w mainie");
+        System.out.println("jestem piesem w mainie");
+        System.out.println("jestem piesem w mainie");
     }
-
 
 
 }
