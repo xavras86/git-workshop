663c480ca1875b6344d916daee62a719a60663a8
Marcin Sikora <sikomar@interia.pl>
2023-04-03 19:19:40 +0200

    second new branchcommit
    # Conflicts:
    #	Dog.java

---
+++ Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    public final int age;
+    public final String name;
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
