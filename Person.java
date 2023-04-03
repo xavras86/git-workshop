32480851156c6af4ae6a4f410b118dc6f030e1dd
Marcin Sikora <sikomar@interia.pl>
2023-04-03 19:56:22 +0200

    Person2

--- Person.java
+++ Person.java
@@ -3,6 +3,8 @@ public class Person {
     public static void main(String[] args) {
 
         System.out.println("HEllo GIT!");
+        System.out.println("HEllo GIT!");
+        System.out.println("HEllo GIT!");
         int asd = 345;
         int a =3778;
 
